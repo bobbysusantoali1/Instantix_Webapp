b0fce1eb547e5a6758e5408451d20fe5cae84a1f
RioPramana21 <73391102+RioPramana21@users.noreply.github.com>
2023-01-25 02:31:25 +0700

    changed variable name

--- app/Http/Controllers/HomePage.php
+++ app/Http/Controllers/HomePage.php
@@ -8,7 +8,7 @@ use App\Models\event;
 class HomePage extends Controller
 {
     public function view(){
-        $data = event::all();
+        $events = event::all();
         return view('Components.Home',[
             'title' => 'Home Page InstanTix',
             'events' => $events,
