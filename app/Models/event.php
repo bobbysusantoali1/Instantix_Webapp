04b676cb6578ef9180d5dd76f75ca98f186eb951
thelimits <prambudi150@gmail.com>
2023-01-24 19:02:33 +0700

    update library models

--- app/Models/event.php
+++ app/Models/event.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
 class event extends Model
 {
