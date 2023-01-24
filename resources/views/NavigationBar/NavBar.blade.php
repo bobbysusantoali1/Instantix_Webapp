752ebedcaee63f0141c88465b65098f6a2676486
williamt801 <105440959+williamt801@users.noreply.github.com>
2023-01-24 15:18:53 +0700

    LoginPage
    -

--- resources/views/NavigationBar/NavBar.blade.php
+++ resources/views/NavigationBar/NavBar.blade.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{url('../CSS/style.css') }}">
+    @yield('css')
     <title>@yield('title')</title>
 </head>
 
