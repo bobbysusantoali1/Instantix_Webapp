c3d33a7674945b14381261f3a112a9a54153cc97
RioPramana21 <73391102+RioPramana21@users.noreply.github.com>
2023-01-25 11:41:34 +0700

    Added code to view profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function view(){
+        $curr_user = Auth::user();
         return view('Components.MyProfile',[
             'title' => 'My Profile',
+            'user' => $curr_user
         ]);
     }
 }
--- resources/views/Components/MyProfile.blade.php
+++ resources/views/Components/MyProfile.blade.php
@@ -1,7 +1,30 @@
 @extends('MainBody')
 @section('title', $title)
 @section('content')
-<div class="row p-5 d-flex justify-content-center">
-
+<div class="container bg-light">
+    <div class="card-body d-flex align-items-center flex-column">
+        <h1 class="card-title text-black my-4">My Profile</h1>
+        <div class="container my-1 d-flex align-items-center flex-column">
+            <span class="py-1 px-3 bg-secondary rounded text-white">
+                @if ($user->role == 'customer')
+                    Customer
+                @else
+                    Event Organizer
+                @endif
+            </span>
+            <p class="m-1 card-text fw-bold">Full Name: {{ $user->full_name }}</p>
+            <p class="m-1 card-text ">{{ $user->email }}</p>
+            <p class="m-1 card-text ">Address: {{ $user->address }}</p>
+            <p class="m-1 card-text ">Gender: {{ $user->gender }}</p>
+            <p class="m-1 card-text ">Date of Birth: {{ $user->dob }}</p>
+            <p class="m-1 card-text ">Phone: {{ $user->phone_number }}</p>
+        </div>
+        <div class="container my-3">
+            @if (!$user->role == 'eventOrganizer')
+                {{-- <a href="{{ route('view-edit-profile') }}" class="btn btn-primary px-4">Edit Profile</a> --}}
+            @endif
+            {{-- <a href="{{ route('view-edit-password') }}" class="btn btn-outline-primary px-4">Edit Password</a> --}}
+        </div>
+    </div>
 </div>
 @endsection
