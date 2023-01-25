af3f8ef995ba9b7b4ff2a298e44157682e6f4523
Renanda Adrian Putra <renanda1301@gmail.com>
2023-01-25 16:43:55 +0700

    edit event

--- resources/views/eventOrganizer/editEvent.blade.php
+++ resources/views/eventOrganizer/editEvent.blade.php
@@ -2,21 +2,20 @@
 @section('title', 'Dashboard')
 
 @section('content-css')
-    <link rel="stylesheet" href="./../../css/eventOrganizer/eventDetail.css">
+    <link rel="stylesheet" href="./../../css/eventOrganizer/editEvent.css">
 @endsection
 
-@section('dashboard-content-title', 'Event Detail')
+@section('dashboard-content-title', 'Edit Event')
 
 @section('dashboard-content')
-    <div class="d-flex flex-row justify-content-start align-items-start mb-2 h-100"
-        style="border: 1px solid rgba(0, 0, 0 , 0);">
+    <div class="d-flex flex-row justify-content-start align-items-start mb-2" style="border: 1px solid rgba(0, 0, 0 , 0);">
         <div style="margin: 24px 40px 20px 20px">
             {{-- <img class="event-image" src="{{ URL::asset('images/' . $event->event_image) }}"
                     alt="{{ $event->event_image }}"> --}}
             <img class="event-image" src="{{ url('storage/app/public/' . $event->event_image) }}"
                 alt="{{ $event->event_image }}">
         </div>
-        <div class="d-flex flex-column justify-content-between align-items-start h-100" style="margin-top:24px">
+        <div class="d-flex flex-column justify-content-between align-items-start" style="margin-top:24px">
             <div class="d-flex flex-column justify-content-start align-items-start" style="gap: 12px">
                 <h2>{{ $event->event_name }}</h2>
                 <div>{{ $event->event_desc }}</div>
@@ -24,14 +23,14 @@
                 <div>Date: {{ $event->event_date }}</div>
                 <div>Time: {{ $event->event_start_time . ' - ' . $event->event_end_time }}</div>
             </div>
-            <div class="d-flex flex-row justify-content-end align-items-center w-100" style="gap: 12px">
+            <div class="d-flex flex-row justify-content-end align-items-center">
                 <button type="button" class="btn btn-outline-danger">Delete</button>
                 <button type="button" class="btn btn-primary">Edit</button>
             </div>
         </div>
     </div>
 
-    <h2 class="mb-2 mt-5">Ticket Types</h2>
+    <h2 class="mb-2">Ticket Types</h2>
     <div class="d-flex flex-column" style="gap: 20px">
         @foreach ($tickets as $ticket)
             <div class="d-flex flex-column justify-content-start align-items-start ticket-card 
